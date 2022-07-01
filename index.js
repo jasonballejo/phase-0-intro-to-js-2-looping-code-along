4ba7af5265f6e7bb347741cd5042a01dbbfd6130
Jason Ballejo <ballejo19@gmail.com>
2022-06-30 20:59:26 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)   
+    }
+    return newArray;
+}
+
+function countDown() {
+    let down = 10; // We will start from 11 (which is 10)
+    while (down > 0)
+        console.log(down--);
+        console.log(down);
+}
